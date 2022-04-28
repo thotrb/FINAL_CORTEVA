f60f0622cc608f4fb136564bcbec53cb356afc6e
thotrb <44053271+thotrb@users.noreply.github.com>
2022-04-28 16:05:54 +0200

    enlever les fleches

---
+++ FINAL_UI/src/components/DashboardComponents/ArrowHorizontale.vue
@@ -0,0 +1,26 @@
+<template>
+  <img src="../../assets/arrowHorizontale.png" height="15px" width="30px"/>
+</template>
+
+<script>
+
+export default {
+
+
+  name: "ArrowHorizontale",
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+  ,
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
+
