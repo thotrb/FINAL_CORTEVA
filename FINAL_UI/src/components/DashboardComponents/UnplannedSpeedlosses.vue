ed929d1d50db17b960e10acc8d2cbfcf4e973b09
gustavo-bertoldi <gg.bertoldi@hotmail.com>
2022-06-01 20:33:53 +0200

    Speed losses duplcates fixed

--- FINAL_UI/src/components/DashboardComponents/UnplannedSpeedlosses.vue
+++ FINAL_UI/src/components/DashboardComponents/UnplannedSpeedlosses.vue
@@ -211,6 +211,13 @@ export default {
               //Add fetched events to the slEvents variable
               //Creates charts' data
               if (this.getSpeedLosses["SLEVENTS"]) {
+                console.log("avant filter", this.getSpeedLosses["SLEVENTS"]);
+                //Remove duplicates
+                this.getSpeedLosses["SLEVENTS"] = this.getSpeedLosses["SLEVENTS"].filter((sl, index, self) => {
+                  return self.findIndex(v => v.OLE === sl.OLE) === index;
+                });
+                console.log("après filter", this.getSpeedLosses["SLEVENTS"]);
+
                 this.slEvents = this.getSpeedLosses["SLEVENTS"].reduce((acc, slEvent) => {
                   if (acc[slEvent.reason]) {
                     //If event is concerned by a chart, create its data
